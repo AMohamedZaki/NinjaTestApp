96be2502913ce6bb196d8900deff3b8efbe284f6
Abdelmn3m mohamed <abdelmoneim.mohamed@nt-me.com>
2020-12-20 02:12:42 +0200

    Simple Example of Testing Void Method

---
+++ NinjaApp.UnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TestNinja.Fundamentals;
+
+namespace NinjaApp.UnitTests
+{
+    [TestFixture]
+    public class ErrorLoggerTests
+    {
+
+        [Test]
+        public void Log_WhenCalled_LastLogShouldSaveTheLog()
+        {
+            // Assign 
+            var log = new ErrorLogger();
+
+            // Act 
+            log.Log("a");
+
+            // Assert
+
+            Assert.That(log.LastError, Is.EqualTo("A").IgnoreCase);
+
+        }
+    }
+}
